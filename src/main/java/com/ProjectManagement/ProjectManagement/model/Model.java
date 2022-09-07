29e56025f04bf15025eacee0498275bc3af7b1db
CodexBox <dineshreddy1590@gmail.com>
2022-09-07 16:00:03 +0530

    done

--- src/main/java/com/ProjectManagement/ProjectManagement/model/Model.java
+++ src/main/java/com/ProjectManagement/ProjectManagement/model/Model.java
@@ -5,10 +5,6 @@ import lombok.Data;
 import java.util.Date;
 
 @Data
-import lombok.Data;
-import org.springframework.context.annotation.Configuration;
-
-
 public class Model {
 
     private Date date;
