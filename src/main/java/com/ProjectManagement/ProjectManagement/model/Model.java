ec5071cf019cbb09a57530b3b29ee37ebe979ad0
CodexBox <dineshreddy1590@gmail.com>
2022-09-07 15:06:59 +0530

    int

--- src/main/java/com/ProjectManagement/ProjectManagement/model/Model.java
+++ src/main/java/com/ProjectManagement/ProjectManagement/model/Model.java
@@ -1,4 +1,14 @@
 package com.ProjectManagement.ProjectManagement.model;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Model {
+
+    private Date date;
+    private String status;
+    private String environment;
+
 }
--- src/main/java/com/ProjectManagement/ProjectManagement/repository/Repo.java
+++ src/main/java/com/ProjectManagement/ProjectManagement/repository/Repo.java
@@ -1,4 +1,11 @@
 package com.ProjectManagement.ProjectManagement.repository;
 
-public interface Repo {
+import com.ProjectManagement.ProjectManagement.entity.Entity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Repo extends CrudRepository<Entity,String> {
+
+    Entity findByTitle(String title);
 }
